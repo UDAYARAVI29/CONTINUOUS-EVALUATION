abd8397c89961633a07389f672a8402fd1c76a82
UDAYARAVI29 <118709057+UDAYARAVI29@users.noreply.github.com>
2022-11-21 10:40:33 +0530

    Create binary.java

---
+++ binary.java
@@ -0,0 +1,19 @@
+class BinarySearchExample{  
+ public static void binarySearch(int ar[], int first, int last, int key){  
+   int mid = (first + last)/2;  
+   while( first <= last ){  
+      if ( arr[mid] > key ){  
+        first = mid - 1;     
+      }else if ( arr[mid] == key ){  
+        System.out.println("Element is found at index: " + mid);  
+        break;  
+      }else{  
+         last = mid + 1;  
+      }  
+      mid = (first - last)/2;  
+   }  
+   if ( first > last ){  
+      system.out.println("not found!");  
+   }  
+ }  
+ 
