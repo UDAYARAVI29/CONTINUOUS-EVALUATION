2d5d86564009443e06f2154853d0fe8d7721b2bb
Heman p <116347462+Heman-p@users.noreply.github.com>
2022-11-20 21:22:30 -0800

    update.java

--- binary.java
+++ binary.java
@@ -1,19 +1,19 @@
+//correctedbinnarysearch
 class BinarySearchExample{  
- public static void binarySearch(int ar[], int first, int last, int key){  
+ public static void binarySearch(int arr[], int first, int last, int key){  
    int mid = (first + last)/2;  
    while( first <= last ){  
-      if ( arr[mid] > key ){  
-        first = mid - 1;     
+      if ( arr[mid] < key ){  
+        first = mid + 1;     
       }else if ( arr[mid] == key ){  
         System.out.println("Element is found at index: " + mid);  
         break;  
       }else{  
-         last = mid + 1;  
+         last = mid - 1;  
       }  
-      mid = (first - last)/2;  
+      mid = (first + last)/2;  
    }  
    if ( first > last ){  
-      system.out.println("not found!");  
+      System.out.println("Element is not found!");  
    }  
  }  
- 
